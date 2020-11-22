ceccbbdf6a08df5b55047710b26ae6e594704890
mayssatbr <64826384+mayssatbe@users.noreply.github.com>
2020-11-21 22:37:42 -0500

    commit

--- MyApplication3/app/src/main/java/com/example/myapplication/Services.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Services.java
@@ -11,16 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class Services extends AppCompatActivity {
-    DatabaseReference mRef;
+
 
     ListView myListView;
     Button addvalue;
@@ -34,53 +29,11 @@ public class Services extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.services);
 
-        mRef = FirebaseDatabase.getInstance().getReference();
-
-        myArrayAdapter = new ArrayAdapter<String>(Services.this,android.R.layout.simple_list_item_1, myArrayList);
-
-        myListView = (ListView) findViewById(R.id.listViewProducts);
-        addvalue = (Button) findViewById(R.id.addButton);
-        servName = (EditText) findViewById(R.id.Service_Name);
-
-        myListView.setAdapter(myArrayAdapter);
-
-        addvalue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mRef.push().setValue(servName.getText().toString());
-            }
-        });
 
 
 
-        mRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String value = snapshot.getValue(String.class);
-                myArrayList.add(value);
-                myArrayAdapter.notifyDataSetChanged();
 
-            }
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 }
